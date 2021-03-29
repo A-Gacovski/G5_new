253547b52f929f6af1e6e0735ed32e1f7974cad3
Aleksandar Gacovski <gacovskialeksandar@gmail.com>
2021-03-29 20:07:22 +0100

    homework & exercises

---
+++ C# Basic/exercises-2/exercises_2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace exercises_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // exercise#1
+            double doubleVar1;
+            double doubleVar2;
+            doubleVar1 = 12.285;
+            doubleVar2 = 3.085;
+            double doubleDivision = doubleVar1 / doubleVar2;
+
+            int intNum1 = 66;
+            int intNum2 = 33;
+            int intDivision = intNum1 / intNum2;
+
+            Console.WriteLine("first divided number is " + doubleDivision);
+            Console.WriteLine($"second divided number is {intDivision}");
+
+            // exercise#2
+            string name = "Aleksandar";
+            string secondName = "Gacovski";
+            string fullName = name + " " + secondName;
+
+            string firstNumber = "9";
+            string secondNumber = "3";
+            string concatinate = firstNumber + secondNumber;
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(concatinate);
+
+            // exercise#3
+            int newNumber = 323;
+            string newString = "232";
+            string newConcatination = newNumber + newString;
+            Console.WriteLine("the new string is:" + newConcatination);
+
+            // exercise#4
+            int n = 102;
+            int m = 5;
+            int result = n / m;
+            Console.WriteLine(result + " messages can be sent");
+
+            // exercise#5
+            int branches = 12;
+            int apples = 8;
+            int baskets = 5;
+            Console.WriteLine("please enter number of trees");
+            string trees = Console.ReadLine();
+            int outVar;
+            int addResult;
+            bool checkTrees = int.TryParse(trees, out outVar);
+            if ((outVar % baskets == 0) && (checkTrees = true))
+            {
+                addResult = outVar * branches * apples / baskets;
+            }
+            else
+            {
+                addResult = outVar * branches * apples / baskets + 1;
+            }
+
+            Console.WriteLine(addResult + " baskets needed to collect the apples");
+
+            // exercise#6
+            Console.WriteLine("please enter first number");
+            string lateVar1 = Console.ReadLine();
+            int lateNumber1 = int.Parse(lateVar1);
+
+            Console.WriteLine("please enter second number");
+            string lateVar2 = Console.ReadLine();
+            int lateNumber2 = int.Parse(lateVar2);
+
+            if (lateNumber1 > lateNumber2)
+            {
+                Console.WriteLine("larger number of two is " + lateNumber1);
+            }
+            else if (lateNumber1 < lateNumber2)
+            {
+                Console.WriteLine("larger number of two is " + lateNumber2);
+            }
+            else
+            {
+                Console.WriteLine("the two numbers are equal");
+            }
+
+            if (lateNumber1 % 2 == 0)
+            {
+                Console.WriteLine("the number " + lateNumber1 + " is even");
+            }
+            else
+            {
+                Console.WriteLine("the number " + lateNumber1 + " is odd");
+            }
+
+            if (lateNumber2 % 2 == 0)
+            {
+                Console.WriteLine("the number " + lateNumber2 + " is even");
+            }
+            else
+            {
+                Console.WriteLine("the number " + lateNumber2 + " is odd");
+            }
+
+            // exercise#7
+            Console.WriteLine("please enter number from 1 to 3");
+            string addNumber = Console.ReadLine();
+
+            switch (addNumber)
+            {
+                case "1":
+                    Console.WriteLine("you've got a new car");
+                    break;
+                case "2":
+                    Console.WriteLine("you've got a new plane");
+                    break;
+                case "3":
+                    Console.WriteLine("you've got a new bike");
+                    break;
+                default:
+                    Console.WriteLine("you haven't got any machine");
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ C# Basic/homework-2/homework-2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // homework-2_exercise_1
+            Console.WriteLine("please enter first number");
+            string newInput1 = Console.ReadLine();
+            double newNumber1 = double.Parse(newInput1);
+
+            Console.WriteLine("please enter second number");
+            string newInput2 = Console.ReadLine();
+            double newNumber2 = double.Parse(newInput2);
+
+            Console.WriteLine("please enter operation to calculate");
+            string operInput = Console.ReadLine();
+
+
+            switch (operInput)
+            {
+                case "+":
+                    Console.WriteLine($"The result is {newNumber1 + newNumber2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"The result is {newNumber1 - newNumber2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"The result is {newNumber1 * newNumber2}");
+                    break;
+                case "/":
+                    Console.WriteLine($"The result is {newNumber1 / newNumber2}");
+                    break;
+                default:
+                    Console.WriteLine("sorry there is no such operation");
+                    break;
+            }
+
+            // homework-2_exercise_2
+            Console.WriteLine("please enter No.1");
+            string addInput1 = Console.ReadLine();
+            double addNumber1 = int.Parse(addInput1);
+
+            Console.WriteLine("please enter No.2");
+            string addInput2 = Console.ReadLine();
+            double addNumber2 = int.Parse(addInput2);
+
+            Console.WriteLine("please enter No.3");
+            string addInput3 = Console.ReadLine();
+            double addNumber3 = int.Parse(addInput3);
+
+            Console.WriteLine("please enter No.4");
+            string addInput4 = Console.ReadLine();
+            double addNumber4 = int.Parse(addInput4);
+
+            double addResult = (addNumber1 + addNumber2 + addNumber3 + addNumber4) / 4;
+            Console.WriteLine($"The average of {addNumber1}, {addNumber2}, {addNumber3} and {addNumber4} is: {addResult}");
+
+            // homework-2_exercise_3
+            Console.WriteLine("please input the first number");
+            string lateInput1 = Console.ReadLine();
+            int lateNumber1 = int.Parse(lateInput1);
+
+            Console.WriteLine("please input the second number");
+            string lateInput2 = Console.ReadLine();
+            int lateNumber2 = int.Parse(lateInput2);
+
+            int firstVarToSec = lateNumber2;
+            int secondVarToFirst = lateNumber1;
+
+            lateNumber1 = firstVarToSec;
+            lateNumber2 = secondVarToFirst;
+
+            Console.WriteLine("first number: " + lateNumber1);
+            Console.WriteLine("second number: " + lateNumber2);
+
+
+
+        }
+    }
+}
