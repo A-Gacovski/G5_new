ca1380ec621e641e402d85a72c45cbed6b11afda
A-Gacovski <77409102+A-Gacovski@users.noreply.github.com>
2021-04-26 20:50:57 +0200

    homework

---
+++ C# Advanced/homework-1/Homework-1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+
+            Console.WriteLine("Please enter words, to stop enter x: ");
+            string newWord = Console.ReadLine();
+            while (newWord.ToLower() != "x")
+            {
+                words.Add(newWord);
+                newWord = Console.ReadLine();
+            }
+
+            Console.WriteLine("Please enter some text: ");
+            string newParagraph = Console.ReadLine();
+
+            string midiParagraph = newParagraph.ToLower();
+            string [] paragraph = midiParagraph.Split(" ");
+
+            int count = 0;
+            words.ForEach(word =>
+            {
+                paragraph.ForEach(cut =>
+                {
+                    if (word == cut)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    
+                });
+
+            });
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
