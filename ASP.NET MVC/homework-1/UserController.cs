60bed2f172d242c9cedf06c948940205b9a5a17d
A-Gacovski <77409102+A-Gacovski@users.noreply.github.com>
2021-08-09 22:59:20 +0200

    homework#1

---
+++ ASP.NET MVC/homework-1/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppHomework.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet("localhost:1111/User")]
+        public IActionResult GetUser()
+        {
+            return View("User");
+        }
+
+        public IActionResult GetContact()
+        {
+            return RedirectToAction("Admin", "Admin");
+        }
+
+        [HttpGet("localhost:1111/Empty")]
+        public IActionResult GetEmpty()
+        {
+            return new EmptyResult();
+        }
+
+        [HttpGet("localhost:1111/Id/{id}")]
+        public IActionResult GetId(int id)
+        {
+            if (id == 2)
+            {
+                return RedirectToAction("GetEmpty");
+            }
+            else
+            {
+                return View("User");
+            }
+            
+         
+        }
+        
+    }
+}
