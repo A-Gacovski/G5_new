d7c18df88891bb8827602f82547d71522ec9b103
A-Gacovski <77409102+A-Gacovski@users.noreply.github.com>
2021-08-19 15:17:31 +0200

    homework-3 class-04

---
+++ ASP.NET MVC/homework-3/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework_3.Models
+{
+    public class Order
+    {
+        public int? Id { get; set; }
+
+        public User User { get; set; }
+
+        public string Pizza { get; set; }
+
+        public double Price { get; set; }
+
+        public bool Delivered { get; set; }
+    }
+}
---
+++ ASP.NET MVC/homework-3/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework_3.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public long Phone { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
