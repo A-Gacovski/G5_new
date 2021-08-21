def701a105928ffbdfbe68c3e091e77ddf676169
A-Gacovski <77409102+A-Gacovski@users.noreply.github.com>
2021-08-21 11:26:09 +0200

    Unfinished classwork

---
+++ ASP.NET MVC/Class07/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Class07.Models;
+using Class07.Models.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class07.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+
+            var employee = new Employee
+            {
+                EmployeeId = 21,
+                FirstName = "Aleksandar",
+                LastName = "Gacovski",
+                Salary = 1500,
+                Position = Position.Junior
+            };
+            var employeeViewModel = new EmployeeViewModel()
+            {
+                EmployeeId = employee.EmployeeId,
+                FullName = employee.FullName(),
+                Position = Position.Junior
+            }
+        }
+    }
+}
---
+++ ASP.NET MVC/Class07/Models/Enums/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class07.Models.Enums
+{
+    public enum Position
+    {
+        Junior,
+        Intermediate,
+        Senior,
+        TeamLeader
+    }
+}
